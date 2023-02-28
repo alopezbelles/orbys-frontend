01d6ef5ccb9664c3e9df03d0063f8af89af7f049
Alex <alopezbelles@gmail.com>
2023-02-28 22:47:23 +0100

    create the structure of the components and add paths to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './Components/Header/Header';
+import Books from './Containers/Books/Books';
+import BookDetail from './Containers/BookDetail/BookDetail';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <BrowserRouter>
+      
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books/>} />
+        <Route path="/bookdetail" element={<BookDetail/>} />
+                
+      </Routes>
+    </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/Containers/BookDetail/BookDetail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./BookDetail.css";
+
+const BookDetail = () => {
+  return (
+    <div>
+      <h1>Book Detail</h1>
+    </div>
+  );
+};
+
+export default BookDetail;
