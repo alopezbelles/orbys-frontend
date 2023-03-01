8cf921a9410b3c2ce936fe3111b00e0457ce4389
Alex <alopezbelles@gmail.com>
2023-03-01 13:48:48 +0100

    working on home layout

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import "./Header.css";
 
 import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import logoNavbar from '../../assets/logo.navbar.png';
+
 
 
 const Header = () => {
   return (
     <AppBar position="static">
-  <Toolbar>
-    <Typography variant="h6" style={{ flexGrow: 1 }}>
-      Mi sitio web
-    </Typography>
-    <Button color="inherit">Inicio</Button>
-    <Button color="inherit">Acerca de</Button>
-    <Button color="inherit">Contacto</Button>
+    <Toolbar className="toolbarDesign">
+      <img className="logoNavbar" src={logoNavbar} alt="logo"></img>
+
+    {/* <Typography className="logoNavbar" variant="h6" style={{ flexGrow: 1 }}>
+      BOOK-ZONE
+    </Typography> */}
+    <Button>Home</Button>
+    <Button >Books</Button>
   </Toolbar>
 </AppBar>
 
--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import "./Home.css";
 
+import logo from '../../assets/logo.png';
+import vector1 from '../../assets/vector1.png';
+import vector2 from '../../assets/vector2.png';
+import vector3 from '../../assets/vector3.png';
+import vector4 from '../../assets/vector4.png';
+// import logo2 from '../../assets/logo2.png';
+
+import { Container, Box } from "@material-ui/core";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Box width="100%" className="boxHome">
+      {/* <h1>Home</h1> */}
+
+      <img className="logoDesign" src={logo} alt="logo"></img>
+
+      <div className="description">
+        <p className="paragraph1">BOOK-ZONE is an online bookstore offering a wide selection of books in various languages and genres. From classic literature to the latest bestsellers, Book-zone has something for every reader. The platform features a user-friendly interface that allows users to search for, and browse books in a matter of minutes.</p>
+
+        <div className="vectorBox">
+
+        <img className="vectorDesign" src={vector1} alt="vector"></img>
+        <img className="vectorDesign" src={vector2} alt="vector"></img>
+        <img className="vectorDesign" src={vector3} alt="vector"></img>
+        <img className="vectorDesign" src={vector4} alt="vector"></img>
+
+        </div>
+         </div>
+
+      
+    </Box>
   );
 };
 
